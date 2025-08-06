27b1dd9e467ddf8ba6c4f9801208599f050b554b
RustyTaint666 <konnercurtis11@gmail.com>
2025-08-06 02:11:13 -0300

    Fruit Table Goal 3

--- src/app/fruit-challenge/components/fruit-table/fruit-table.component.ts
+++ src/app/fruit-challenge/components/fruit-table/fruit-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FruitTableViewModel } from './fruit-table-view-model';
 import { FruitSortOption } from "../../enums/fruit-sort-option";
+import {Fruit} from "../../models/fruit";
 
 interface SortOptionDisplay {
   label: string;
@@ -53,5 +54,27 @@ export class FruitTableComponent implements OnInit {
    */
   applySort(): void {
     this.viewModel.updateSortOption(this.sortOption);
+  }
+
+  /**
+   * Goal 3: Checks if 'calories' column value is less than or equal to 50.
+   * Used in HTML to apply the 'bold-calories' class.
+   * @param calories The calorie value of a fruit.
+   * @returns True if calories are 50 or less, false otherwise.
+   */
+  isLowCalorie(calories: number): boolean {
+    return calories <= 50;
+  }
+
+  /**
+   * Goal 3: Checks if the entire row should have the special background color.
+   * Criteria: 'calories' column value is less than or equal to 50 AND
+   * 'sugar' column value is greater than or equal to 8.
+   * Used in HTML to apply the 'special-row' class.
+   * @param fruit The Fruit object for the current row.
+   * @returns True if the fruit meets the special row criteria, false otherwise.
+   */
+  isSpecialRow(fruit: Fruit): boolean {
+    return fruit.nutritions.calories <= 50 && fruit.nutritions.sugar >= 8;
   }
 }
