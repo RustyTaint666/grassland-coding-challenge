81dc30eecb4f511754f29ff4ff04cabeed682544
RustyTaint666 <konnercurtis11@gmail.com>
2025-08-06 13:51:42 -0300

    Messaging Goal 3

--- src/app/messaging-challenge/components/messaging/messaging.view-model.ts
+++ src/app/messaging-challenge/components/messaging/messaging.view-model.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {MessagingService} from "../../services/messaging.service";
 import {Message} from "../../models/message";
+import {TextMessage} from "../../models/text-message";
+import {ImageMessage} from "../../models/image-message";
 
 @Injectable({
     providedIn: 'root'
@@ -11,5 +13,15 @@ export class MessagingViewModel {
 
     constructor(private messagingService: MessagingService) {
         this.messages$ = this.messagingService.messages$;
+    }
+
+    /**
+     * Helper method to check if a message is a TextMessage.
+     * This logic is now in the ViewModel.
+     * @param message The message object to check.
+     * @returns True if the message is a TextMessage, false otherwise.
+     */
+    isTextMessage(message: Message): message is TextMessage {
+        return message instanceof TextMessage;
     }
 }
